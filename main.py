1c30188b16c96e7354b643e79e03c68e0cd9179f
Jhon <jhonatasousa288@gmail.com>
2024-10-23 22:10:35 -0300

    v0.7

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import pywhatkit as kit
 from os import getenv
 from datetime import datetime
 from dotenv import load_dotenv
-from requests import get
+from requests import get, post
 from bs4 import BeautifulSoup
 from locale import setlocale, LC_TIME
-from unidecode import unidecode
+
 
 setlocale(LC_TIME, 'pt_BR.UTF-8')
 load_dotenv()
 
-GROUP_NAME = getenv("GROUP_NAME")
+TOKEN = getenv("TOKEN") 
+GROUP_ID = getenv("GROUP_ID")
+API_URL = getenv("API_URL") + '/waInstance' + getenv("ID_INSTANCE") + "/sendMessage/" + TOKEN
+ 
 URL = "https://docs.google.com/spreadsheets/d/1YvCqBrNw5l4EFNplmpRBFrFJpjl4EALlVNDk3pwp_dQ/pubhtml"
+
 
 timeNow = datetime.now()
 hour = timeNow.hour
-minute = timeNow.minute + 2
+minute = timeNow.minute
 weekday = timeNow.weekday()
 res = get(URL)
 msg = ''
@@ -34,27 +37,46 @@ if res.status_code == 200:
     for row in rows[10:-2]:
         dataJantar.append(" ".join(row.find_all('td')[weekday + 1].text.split()))
 
-    msg = f'''Hoje e {timeNow.strftime("%A")} e temos de cardapio:
-*Almoco*
+    msg = f'''Hoje e {timeNow.strftime("%A")} e temos de cardápio:
+*Almoço*
 * Entrada: {dataAlmoco[0]}
-Prato Principal: {dataAlmoco[1]}
-Prato Vegano: {dataAlmoco[2]}
-Guarnicao: {dataAlmoco[3]}
-Acompanhamentos: {dataAlmoco[4]}
-Sobremesa: {dataAlmoco[5]}
+* Prato Principal: {dataAlmoco[1]}
+* Prato Vegano: {dataAlmoco[2]}
+* Guarnição: {dataAlmoco[3]}
+* Acompanhamentos: {dataAlmoco[4]}
+* Sobremesa: {dataAlmoco[5]}
 
 *Jantar*
 * Entrada: {dataJantar[0]}
-Prato Principal: {dataJantar[1]}
-Prato Vegano: {dataJantar[2]}
-Guarnicao: {dataJantar[3]}
-Acompanhamentos: {dataJantar[4]}
-Sobremesa: {dataJantar[5]}
+* Prato Principal: {dataJantar[1]}
+* Prato Vegano: {dataJantar[2]}
+* Guarnição: {dataJantar[3]}
+* Acompanhamentos: {dataJantar[4]}
+* Sobremesa: {dataJantar[5]}
 
 
-O cardapio podera sofrer alteracao sem comunicacao previa.
-Nossas preparacoes podem conter gluten.'''
-    
-    msg = unidecode(msg)
+O cardápio poderá sofrer alteração sem comunicação prévia.
+Nossas preparações podem conter glúten.'''
 
-    kit.sendwhatmsg_to_group(GROUP_NAME, msg, hour, minute)
+
+    data = {
+        "chatId": GROUP_ID,
+        "message": msg
+    }
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    response = post(API_URL, json=data, headers=headers)
+
+    if response.status_code == 200:
+        with open('main.log', 'a') as file:
+            file.write(f'[OK] {timeNow}. Mensagem enviada\n')
+    else:
+        with open('main.log', 'a') as file:
+            file.write(f'[ERRO] {timeNow}. {response.status_code}. {response.text}\n')
+else:
+    with open('main.log', 'a') as file:
+        file.write(f'[ERRO] {timeNow}. Acessar a tabela\n')
